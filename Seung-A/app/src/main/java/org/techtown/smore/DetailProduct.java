491b4be8ad953304a6f788153ee78978f914eab5
Seung-a-oh <osa0215@naver.com>
2021-08-04 03:16:11 +0900

    [승아] - 액티비티 전환

--- Seung-A/app/src/main/java/org/techtown/smore/DetailProduct.java
+++ Seung-A/app/src/main/java/org/techtown/smore/DetailProduct.java
@@ -2,7 +2,12 @@ package org.techtown.smore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class DetailProduct extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class DetailProduct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_product);
+
+        Button ReviewButton = findViewById(R.id.ReviewButton);
+        ReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailProduct.this,ProductReview.class);
+                startActivity(intent);
+            }
+        });
+//        for(int i=1; i<=4; i++){
+//            ImageView imageView = ImageView();
+//            String photo_name = "mamaforest_detailcut"+i;
+//            imageView.setBackgroundResource(R.drawable.);
+//        }
     }
 }
